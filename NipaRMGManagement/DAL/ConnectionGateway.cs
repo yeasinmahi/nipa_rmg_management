8846f62abf6112797666a9dae8b1a7227f4cc941
hossainalazad <hossainalazad@gmail.com>
2016-05-26 17:44:34 +0600

    addinng connectionGateway and configure to db

---
+++ NipaRMGManagement/DAL/ConnectionGateway.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+
+namespace NipaRMGManagement.DAL
+{
+    public class ConnectionGateway
+    {
+        public string ConnectionString = WebConfigurationManager.ConnectionStrings["NipaRMGConnectionString"].ConnectionString;
+        public SqlConnection Connection { get; set; }
+        public SqlCommand Command { get; set; }
+        public SqlDataReader Reader { get; set; }
+        public string Query { get; set; }
+
+        public ConnectionGateway()
+        {
+            Connection = new SqlConnection(ConnectionString);
+            Command = new SqlCommand();
+            Command.Connection = Connection;
+        }
+    }
+}
