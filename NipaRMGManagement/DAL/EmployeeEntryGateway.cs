26e31d420f5171fdddc14aaddb90f49e8cf73fd1
hossainalazad <hossainalazad@gmail.com>
2016-05-27 12:22:31 +0600

    adding bll dal

---
+++ NipaRMGManagement/DAL/EmployeeEntryGateway.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NipaRMGManagement.DAL
+{
+    public class EmployeeEntryGateway
+    {
+    }
+}
